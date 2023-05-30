b8f9327923ff00e80eb2c6ea763e7cfb9a408960
Thabang Gideon Magaola <gideon877@live.com>
2023-05-30 10:37:59 +0200

    partialDirectory

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
 import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path';
 
 export default {
   plugins: [handlebars({
-    context : {
-        title : "Hello this is some text",
-        show : false,
-        fruits : [
-            "apples",
-            "oranges",
-            "bananas"
-        ],
-        fruitSales : [
-            {day : "Monday", apples : 13, oranges : 34},
-            {day : "Tuesday", apples : 17, oranges : 28},
-            {day : "Thursday", apples : 7, oranges : 45},
-        ]
-    }
+    context: {
+      title: "Hello, this is some text",
+      show: false,
+      fruits: [
+        "apples",
+        "oranges",
+        "bananas"
+      ],
+      fruitSales: [
+        { day: "Monday", apples: 13, oranges: 34 },
+        { day: "Tuesday", apples: 17, oranges: 28 },
+        { day: "Thursday", apples: 7, oranges: 45 },
+      ]
+    },
+    partialDirectory: resolve(__dirname, 'src/partials'),
+    // Add any additional options or configurations you need
   })],
 };
